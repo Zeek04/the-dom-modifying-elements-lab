62d54e14049735bf7dfec65002642f614933efee
Ezequiel <cruzezequiel81@gmail.com>
2025-01-09 23:25:15 -0500

    The Dom Modifying Elements Lab Finished

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const mainHtml = document.getElementById("main")
+const newHeader = document.createElement('h1')
+
+
+mainHtml.remove();
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.append(newHeader);
